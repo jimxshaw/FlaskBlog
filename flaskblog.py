aafd4ea65d576e4c27565715d9d9c79450f3d9c8
Jim Shaw <jimxshaw@gmail.com>
2020-04-09 20:16:06 -0500

    Add sample blog posts and display them in home template.

--- flaskblog.py
+++ flaskblog.py
@@ -2,11 +2,26 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        "author": "James Madison",
+        "title": "US Constitution",
+        "content": "The Articles of Confederation must be replaced.",
+        "date_posted": "January 3, 2020"
+    },
+    {
+        "author": "Troy McClure",
+        "title": "Where are they going?",
+        "content": "They are gone and nobody knows where.",
+        "date_posted": "January 6, 2020"
+    }
+]
+
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", posts=posts)
 
 
 @app.route("/about")

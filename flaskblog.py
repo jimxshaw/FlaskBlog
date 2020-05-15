dac641a8341d5e135b75f1075e969041b7974ed0
Jim Shaw <jimxshaw@gmail.com>
2020-05-14 21:57:17 -0500

    Update Post class.

--- flaskblog.py
+++ flaskblog.py
@@ -26,7 +26,7 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False,
-                            datetime=datetime.utcnow)
+                            default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
 
